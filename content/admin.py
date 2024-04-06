d33dabf019efdb2988c2719e9d470227153a8c0c
Josué Hernández <joheca1998@outlook.com>
2024-04-06 15:28:01 -0600

    creando Comment Model

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Comment
+
+class CommentAdmin(admin.ModelAdmin):
+  readonly_fields = ("created", )
 
 # Register your models here.
+admin.site.register(Comment, CommentAdmin)
